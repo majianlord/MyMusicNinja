037570baca317274c3ce835fb3540e109e7d0f94
majianlord <majianlord@gmail.com>
2018-05-27 19:39:57 -0500

    Inital Check in and Theaming

--- MyMusicNinja/Controllers/HomeController.vb
+++ MyMusicNinja/Controllers/HomeController.vb
@@ -1,4 +1,6 @@
-﻿Public Class HomeController
+﻿Imports System.IO
+
+Public Class HomeController
     Inherits System.Web.Mvc.Controller
 
     Function Index() As ActionResult
@@ -16,4 +18,24 @@
 
         Return View()
     End Function
+
+    Function BlobTest() As ActionResult
+        ViewData("Message") = "Your contact page."
+
+        Return View()
+    End Function
+
+
+    <HttpPost>
+    Public Function UploadFiles(ByVal files As IEnumerable(Of HttpPostedFileBase)) As ActionResult
+        For Each File As HttpPostedFileBase In files
+            Dim filePath As String = Guid.NewGuid().ToString + Path.GetExtension(File.FileName)
+            File.SaveAs(Path.Combine(Server.MapPath("~/UploadedFiles"), filePath))
+        Next
+
+        Return Json("file uploaded successfully")
+    End Function
+
+
+
 End Class
